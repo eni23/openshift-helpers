08a065c0aa753ce42adf64e5d2ebb0d64af7d1a9
Cyrillvw <cyrill.vonwattenwyl@adfinis-sygroup.ch>
2017-03-21 11:16:33 +0100

    fix host

--- setup-wordpress.sh
+++ setup-wordpress.sh
@@ -5,4 +5,4 @@ cp /opt/app-root/src/wp-config-sample.php /opt/app-root/src/wp-config.php
 sed -i "s/database_name_here/wordpress/g" /opt/app-root/src/wp-config.php
 sed -i "s/username_here/wordpress/g" /opt/app-root/src/wp-config.php
 sed -i "s/password_here/${DATABASE_PASSWORD}/g" /opt/app-root/src/wp-config.php
-sed -i "s/localhost/${DATABASE_PASSWORD}/g" /opt/app-root/src/wp-config.php
+sed -i "s/localhost/mysql/g" /opt/app-root/src/wp-config.php
